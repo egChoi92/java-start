8cae65e9c3c674867716805a6c95237b4f76c0f6
egChoi <egChoi92@gmail.com>
2024-02-28 09:46:34 +0900

    2. 변수

---
+++ src/variable/VarEX3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEX3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanVar = true;
+        System.out.println("booleanVar = " + booleanVar);
+    }
+}
